5b4e12ee052d29f580192eb1e9c9f7e5a66f56a2
Michael Grant <mdgrant116@gmail.com>
2020-10-15 18:29:56 -0400

    Initial Commit

---
+++ Project 1 SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 1 SwiftUI
+//
+//  Created by Michael Grant on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
